2a891b9a0b5ec24a5c33f3841688012a776d53a4
Chiao F <php@html.js.cn>
2019-03-09 15:18:10 +0800

    dump databases

--- mysql/dump.sh
+++ mysql/dump.sh
@@ -1,15 +1,45 @@
 #!/bin/bash
 
+CUSTOM_OPTIONS="hc:d:o:"
+
 source "$(dirname "$BASH_SOURCE")"/common.sh
+
+while getopts "$CUSTOM_OPTIONS" opt; do
+  case $opt in
+    c)
+      CONTAINER_ID=${OPTARG}
+      ;;
+    d)
+      DATABASES=${OPTARG}
+      ;;
+    o)
+      OUTPUT=${OPTARG}
+      ;;
+    h)
+      echo "Usage:"
+      echo "  -c container id"
+      echo "  -d database"
+      echo "  -o output"
+      exit 0
+      ;;
+  esac
+done
 
 if [ -z $CONTAINER_ID ]; then
   select_container $DEFAULT_IMAGE
 fi
+
 check_container ${CONTAINER_ID}
 
-if [ -z $OUTPUT ]; then
-  docker exec -it ${CONTAINER_ID} sh -c 'exec mysqldump --single-transaction -A '
+if [ -z "${DATABASES}" ]; then
+  DATABASES_OPTS="exec mysqldump --single-transaction -A "
 else
-  docker exec -it ${CONTAINER_ID} sh -c 'exec mysqldump --single-transaction -A ' > $OUTPUT
+  DATABASES_OPTS="exec mysqldump --single-transaction --databases ${DATABASES}"
+fi
+
+if [ -z "${OUTPUT}" ]; then
+  docker exec -it ${CONTAINER_ID} sh -c "${DATABASES_OPTS}"
+else
+  docker exec -it ${CONTAINER_ID} sh -c "${DATABASES_OPTS}" > $OUTPUT
 fi
 
